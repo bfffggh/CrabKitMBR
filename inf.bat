5b80aacd6d6e3a58a7af54b962d0691b444aa1fa
bfffggh <83696427+bfffggh@users.noreply.github.com>
2023-11-12 15:57:49 +0100

    Update inf.bat

--- inf.bat
+++ inf.bat
@@ -1,7 +1,9 @@
 @echo off
+if exist "%~dp0template.bat" (
 for /f %%A in ('echo %*') do (
 move %%A tmp.exe
 :: "%%~dpnA_.exe"
 "%~dp0bat2exe" /bat "%~dp0template.bat" /exe %%A /invisible /include "%~dp0tmp.exe" /icon "%~dp0crab.ico"
 del "%~dp0tmp.exe"
+)
 )
